3011f19536156604079d09064f11511e7284245d
Dongmin Kim <kdmin0211@gmail.com>
2022-02-12 15:10:18 +0900

    Chore: 기본 redis 의존성 라이브러리 추가

--- app.js
+++ app.js
@@ -1,41 +1,16 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import session from 'express-session';
+
+import redis from 'redis';
+import connectRedis from 'connect-redis';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
-const bodyParser = require('body-parser');
+const redisStore = connectRedis(session);
 
-const redis = require('redis');
-const session = require('express-session');
-const redisStore = require('connect-redis')(session);
-const sessionRouter = require('./route/session');
-
-require('dotenv').config();
-
-const client = redis.createClient(6379, 'localhost');
-
-client.on('error', function (err) {
-  console.log(`Connection Error[redis]${err}`);
-});
-
-client.on('connect', function () {
-  console.log('Complete Connect To Redis');
-});
-
-const sessionInfo = {
-  secret: process.env.EXPRESS_SESSION_SECRET,
-  store: new redisStore({
-    client,
-    ttl: 3600,
-  }),
-  resave: false,
-  saveUninitialized: true,
-};
-
-app.use(session(sessionInfo));
-app.use(bodyParser.json());
-
-app.use('/session', sessionRouter);
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+const client = redis.createClient(process.env.REDIS_PORT);
