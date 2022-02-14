fc1a543483e8dab7b5ec7864a460f1bd6f1cf428
Dongmin Kim <kdmin0211@gmail.com>
2022-02-14 17:39:54 +0900

    Chore: eslint, prettier, typesciprt 설정 추가

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import * as express from 'express';
+
 const app: express.Application = express();
 
 // get
 app.get('/', (req: express.Request, res: express.Response) => {
-    res.send('hello express');
+  res.send('hello express');
 });
 
 // 3010 포트로 서버 실행
 app.listen(3010, () => {
-    console.log('실행중');
+  console.log('실행중');
 });
