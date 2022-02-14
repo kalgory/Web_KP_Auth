f72f5ddd311a74b390bb7a8c448b81c249b2f3f4
Dongmin Kim <kdmin0211@gmail.com>
2022-02-14 18:32:25 +0900

    Chore: express loader 추가

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,11 +1,14 @@
 import * as express from 'express';
 import * as dotenv from 'dotenv';
+
 import serverLoader from 'src/loaders/serverLoader';
+import expressLoader from 'src/loaders/expressLoader';
 
 dotenv.config();
 
 function loaders(app: express.Application) {
   serverLoader(app);
+  expressLoader(app);
 }
 
 export default loaders;
