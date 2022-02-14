373838ffa47aab4d70eac68c80e6993d17d1db64
Dongmin Kim <kdmin0211@gmail.com>
2022-02-14 23:45:24 +0900

    Chore: session 타입 및 loader 추가

--- src/app.ts
+++ src/app.ts
@@ -5,3 +5,14 @@ import loaderInit from 'src/loaders';
 const app: express.Application = express();
 
 loaderInit(app);
+
+app.get('/session', (req, res) => {
+  console.log(req.session);
+  req.session.test = '123';
+  res.send();
+});
+
+app.get('/session_test', (req, res) => {
+  console.log(req.session);
+  res.send();
+});
---
+++ src/loaders/redisLoader.ts
@@ -0,0 +1,32 @@
+import * as express from 'express';
+import * as session from 'express-session';
+import * as connectRedis from 'connect-redis';
+import * as Redis from 'ioredis';
+
+function redisLoader(app: express.Application) {
+  const RedisStore = connectRedis(session);
+  const redisOption = {
+    port: typeof process.env.REDIS_PORT !== 'undefined' ? +process.env.REDIS_PORT : 6379,
+    host: process.env.REDIS_HOST ?? 'localhost',
+  };
+  const redisClient = new Redis(redisOption);
+
+  const sessionSecret = process.env.REDIS_SECRET ?? false;
+
+  if (sessionSecret === false) {
+    throw new Error('Undefined Session Secret');
+  }
+
+  redisClient.on('error', (error: NodeJS.ErrnoException) => {
+    console.log(`Redis Error ${error.message}`);
+  });
+
+  app.use(
+    session({
+      store: new RedisStore({ client: redisClient }),
+      saveUninitialized: false,
+      secret: sessionSecret,
+      resave: false,
+    }),
+  );
+}
