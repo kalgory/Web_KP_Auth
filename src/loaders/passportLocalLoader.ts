fbc3b52d285817e7a830f8ba48ad4449954642af
Dongmin Kim <kdmin0211@gmail.com>
2022-02-16 16:01:36 +0900

    Feat: passport local loader 기본 구조 설정

---
+++ src/loaders/passportLocalLoader.ts
@@ -0,0 +1,13 @@
+import * as express from 'express';
+import * as passport from 'passport';
+import { Strategy, IVerifyOptions } from 'passport-local';
+
+function passportLocalLoader(app: express.Application) {
+  type DoneFunction = (error: any, user: any, options: IVerifyOptions) => void;
+
+  const localStrategyOptions = {
+    usernameField: 'userId',
+    passwordField: 'password',
+  };
+  const verifyLocalUser = async (username: string, password: string, done: DoneFunction) => {};
+}
