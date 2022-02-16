0b5fa212b20fdde9c66d91f89a6533bde578c889
Dongmin Kim <kdmin0211@gmail.com>
2022-02-16 15:34:35 +0900

    Feat: passport loader session middleware 추가

--- src/loaders/passportLoader.ts
+++ src/loaders/passportLoader.ts
@@ -3,6 +3,7 @@ import * as passport from 'passport';
 
 function passportLoader(app: express.Application): void {
   app.use(passport.initialize());
+  app.use(passport.session());
 }
 
 export default passportLoader;
