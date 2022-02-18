7a83a180a05fc5e081f03bdf007dee946fdd55de
Dongmin Kim <kdmin0211@gmail.com>
2022-02-18 14:43:46 +0900

    Feat: random salt 생성 함수 추가 및 hash 생성 함수 추가

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import 'module-alias/register';
 import * as express from 'express';
 import loaderInit from 'src/loaders';
+import * as crypto from 'crypto';
 
 const app: express.Application = express();
 
 loaderInit(app);
+
+console.log(crypto.randomBytes(64));
--- src/loaders/passportLocalLoader.ts
+++ src/loaders/passportLocalLoader.ts
@@ -1,6 +1,33 @@
 import * as passport from 'passport';
 import { Strategy, IVerifyOptions } from 'passport-local';
 import { userService } from 'src/services';
+import * as crypto from 'crypto';
+
+function createSalt(): Promise<string> {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(64, (error, buffer) => {
+      if (error) reject(error);
+      resolve(buffer.toString('base64'));
+    });
+  });
+}
+
+function createPasswordHash(
+  password: string,
+  salt: string,
+): Promise<{ hash: string; salt: string }> {
+  return new Promise((resolve, reject) => {
+    const iterations = process.env.HASH_INTERATIONS;
+    if (typeof iterations === 'undefined' || Number.isNaN(iterations)) {
+      reject(new Error('configuration error'));
+    } else {
+      crypto.pbkdf2(password, salt, parseInt(iterations, 10), 64, 'sha512', (error, hash) => {
+        if (error) reject(error);
+        resolve({ hash: hash.toString('base64'), salt });
+      });
+    }
+  });
+}
 
 function passportLocalLoader() {
   type DoneFunction = (error: any, user?: any, options?: IVerifyOptions) => void;
@@ -13,9 +40,11 @@ function passportLocalLoader() {
   const verifyLocalUser = async (userId: string, password: string, done: DoneFunction) => {
     try {
       const user = await userService.findUserById(userId);
+
       if (!user) {
         return done(null, false, { message: '존재하지 않는 유저입니다.' });
       }
+
       return done(null, user);
     } catch (error) {
       return done(error, false);
